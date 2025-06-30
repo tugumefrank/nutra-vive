381613aff549630c7b22d20513b4015a1987b7ef
tugume frank <frankholmez@gmail.com>
2025-06-30 18:03:23 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const nextConfig = {
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    // your project has ESLint errors fix.
     ignoreDuringBuilds: true,
   },
 };
